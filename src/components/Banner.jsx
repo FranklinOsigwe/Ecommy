50e2994b0a236cc1dfb4876d8a60a685e9643634
franklin <franklin@franklins-MacBook-Air.local>
2024-01-18 00:28:53 +0100

    added hero image

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Banner = () => {
   return (
     <div className='section-container bg-gradient-to-r from-[#FAFAFA] from-0% to-[#FCFCFC] to-100%'>
-        <div className='py-24 flex flex-col md:flex-row justify-between items-center gap-8'>
+        <div className='py-2 flex flex-col md:flex-row justify-between items-center gap-8'>
             <div className='md:w-1/2 space-y-7 px-4'>
                 <h2 className='md:text-5xl text-black text-4xl font-bold md:leading-snug leading-snug'>
                     Dive into delight of Delectable <span className='text-orange-400'>Food</span>.
@@ -11,7 +11,9 @@ const Banner = () => {
                 <p className='text-xl text-[#4A4A4A]'>Where each Plate waves a story of Culinary Mastery and Passionate Craftmanship</p>
                 <button className='btn bg-orange-400 px-8 py-3 text-white font-semibold rounded-full'>Order Now</button>
             </div>
-            <div className='md:w-1/2'>Right</div>
+            <div className='md:w-1/2 chef_img'>
+                <img src='/blackChef.jpg' alt='chef' />
+            </div>
         </div>
     </div>
   )
